8ae44db03d06d514b22bf76e974326704e4624a9
deepuvaranasi <deepthi.varanasi@iths.se>
2023-06-07 17:24:37 +0200

    first commit

---
+++ src/views/BeerList/index.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react';
+import { Beer } from '../../types';
+import { fetchData } from './utils';
+import { Avatar, List, ListItemAvatar, ListItemButton, ListItemText } from '@mui/material';
+import SportsBar from '@mui/icons-material/SportsBar';
+import { useNavigate } from 'react-router-dom';
+import * as React from 'react';
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
+
+const BeerList = () => {
+  const navigate = useNavigate();
+  const [beerList, setBeerList] = useState<Array<Beer>>([]);
+
+  // eslint-disable-next-line
+  useEffect(fetchData.bind(this, setBeerList), []);
+
+  const onBeerClick = (id: string) => navigate(`/beer/${id}`);
+
+  return (
+    <article>
+      <section>
+        <header>
+          <h1>BeerList page</h1>
+        </header>
+        <main>
+          { <List>
+            {beerList.map((beer) => (
+              <ListItemButton key={beer.id} onClick={onBeerClick.bind(this, beer.id)}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <SportsBar />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={beer.name} secondary={beer.brewery_type} />
+              </ListItemButton>
+            ))}
+          </List> 
+          }          
+        </main>
+      </section>
+    </article>
+  );
+};
+
+export default BeerList;
